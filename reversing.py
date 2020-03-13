26d88e1defd1cc307119901042b8ca35c693f524
cymonlix <akatwijukacymonlix@gmail.com>
2020-03-13 15:01:40 +0300

    making some new changes on reversing

---
+++ reversing.py
@@ -0,0 +1,38 @@
+word = input('Word:\n')
+print(word)
+reverse = ''
+for i in word[::-1]:
+  reverse = reverse + i
+print(reverse)
+
+
+
+#recap on conditions
+x = int(input("enter a number of your choice:"))
+if x == 1:
+  print("how are you")
+else:
+  print("hi")
+
+
+#SLICING in python
+lst = [7,2,9,2,5,8]
+for i in lst[2:5]:
+  print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
